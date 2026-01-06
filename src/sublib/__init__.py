7cda35635477674cc444734018277886b49f3a9b
kvnyang <12661173+kvnyang@users.noreply.github.com>
2026-01-06 10:54:23 +0800

    chore: bump version to 0.2.1

--- src/sublib/__init__.py
+++ src/sublib/__init__.py
@@ -6,7 +6,7 @@ Supported formats:
 - ASS (Advanced SubStation Alpha v4+)
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # ASS format - main exports (Facade API)
 from sublib.ass import AssFile, AssEvent, AssStyle
